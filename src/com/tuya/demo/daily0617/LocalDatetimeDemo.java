bd3465dec8a084f02202af45ab75b2c43d10f3ec
chendong <chendong@tuya.com>
2020-06-19 17:58:29 +0800

    the six commit

---
+++ src/com/tuya/demo/daily0617/LocalDatetimeDemo.java
@@ -0,0 +1,21 @@
+package com.tuya.demo.daily0617;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author chendong
+ * @date 2020/6/18 10:43 上午
+ * To change this template use Appearance | Editor | File and Code
+ * Templates.
+ */
+public class LocalDatetimeDemo {
+    public static void main(String[] args) {
+        //当前日期 年-月-日
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        //当前时间 年-月-日-时间
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
+}
--- src/com/tuya/demo/daily0617/StringDemo.java
+++ src/com/tuya/demo/daily0617/StringDemo.java
@@ -13,10 +13,20 @@ import java.util.stream.Stream;
  */
 public class StringDemo {
     public static void main(String[] args) {
-        String str = " 123 , 456 , 789 ";
+        //2147483647为32位int最大值
+        String str = " 1176213349,537778828,2147483647";
         String[] strs = str.split(",");
         System.out.println(Arrays.toString(strs));
-        List<String> strList = Stream.of(strs).map(String::trim).collect(Collectors.toList());
-        System.out.println(strList);
+
+        List<String> list = Arrays.asList(strs);
+
+        //数组 -> stream
+        Stream.of(strs).map(String::trim).map(o -> "tuya" + o + "\t").map(s -> s.substring(4)).
+                collect(Collectors.toList()).forEach(System.out::print);
+        System.out.println();
+        //list -> stream
+        list.stream().map(String::trim).map(Integer::valueOf).sorted((o1, o2) -> o1 - o2)
+                .forEach(System.out::print);
+
     }
 }
